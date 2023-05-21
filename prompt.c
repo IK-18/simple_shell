6e3cb057fe33858c45374357f9ee136368e1e277
IK-18 <ik.arigbe.osula@gmail.com>
2023-05-21 06:02:50 +0100

    corrections to typos and betty errors

--- prompt.c
+++ prompt.c
@@ -10,14 +10,14 @@
 
 void prompt(char **argv, char **environ)
 {
-    // handles address of line in *lineptr and size in n within getline
+    /*handles address of line in *lineptr and size in n within getline*/
     char *lineptr = NULL;
-    int n = 0;
+    size_t n = 0;
 
-    // number of characters read
+    /*number of characters read*/
     ssize_t char_len;
 
-    char *av[] = [ NULL, NULL ];
+    char *av[] = {NULL, NULL};
     pid_t child;
 
     int status, i = 0;
@@ -27,24 +27,24 @@ void prompt(char **argv, char **environ)
         printf("IKShell$ ");
 
         char_len = getline(&lineptr, &n, stdin);
-        // frees the pointer after reading characters
+        /*frees the pointer after reading characters*/
         if (char_len == -1)
         {
             free(lineptr);
             exit(EXIT_FAILURE);
         }
-        // checks for newline character
+        /*checks for newline character*/
         while (lineptr[i])
         {
             if (lineptr[i])
                 lineptr[i] = 0;
         }
-        // stores the user input as av
+        /*stores the user input as av*/
         av[0] = lineptr;
-        // creates a child process to allow the parent process (prompt)
-        //  still run via execve even though processing user input
-        child = fork(); // system call that creates a child process from parent
-        // checks return value of fork
+        /*creates a child process to allow the parent process (prompt)
+        still run via execve even though processing user input*/
+        child = fork(); /*system call that creates a child process from parent*/
+        /*checks return value of fork*/
         if (child == -1)
         {
             free(lineptr);
@@ -52,11 +52,11 @@ void prompt(char **argv, char **environ)
         }
         else if (child == 0)
         {
-            // runs command and checks value simultaneously
+            /*runs command and checks value simultaneously*/
             if (execve(av[0], av, environ) == -1)
                 printf("%s: No such file or directory\n", argv[0]);
         }
-        // monitor the status of the child process and wait
+        /*monitor the status of the child process and wait*/
         else
             wait(&status);
     }
