ecfe9b0cf40fe1c64a2482b4f342d92e4053f2d5
IK-18 <ik.arigbe.osula@gmail.com>
2023-05-22 15:31:00 +0100

    test to fix _strcmp

--- _strcmp.c
+++ _strcmp.c
@@ -9,7 +9,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
+	int res = 1;
 
 	while (*s1 == *s2 && *s1 != '\0')
 	{
@@ -17,6 +17,6 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 	if (s1 != s2)
-		res = *s1 - *s2;
+		res = 0;
 	return (res);
 }
