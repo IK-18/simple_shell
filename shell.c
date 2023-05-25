56492de2c491e87ecc51f4c2bd24ea5a9232f29f
IK-18 <ik.arigbe.osula@gmail.com>
2023-05-25 03:22:06 +0100

    debug

--- shell.c
+++ shell.c
@@ -35,20 +35,20 @@ int main(int argc, char *argv[], char *env[])
 			if (_strcmp("exit", av[0]) == 0)
 			{
 				_puts("exit");
-				if (av[1])
-					exit(_atoi(av[1]));
-				else
-					exit(EXIT_SUCCESS);
+				break;
 			}
-			inbt = inbuilt(av[0]);
-			nc = fchk(av[0]);
-			if (inbt == 0 && nc != NULL)
-				av[0] = nc;
-			path = ptchk(av[0]);
-			if (path == 1)
-				forkexe(av, env);
-			if (nc == NULL && path == 0 && inbt == 0)
-				_puts("its here./shell: No such file or directory\n");
+			/**
+			 * inbt = inbuilt(av[0]);
+			 * nc = fchk(av[0]);
+			 * if (inbt == 0 && nc != NULL)
+			 * 	av[0] = nc;
+			 * path = ptchk(av[0]);
+			 * if (path == 1)
+			 */
+			forkexe(av, env);
+			/** if (nc == NULL && path == 0 && inbt == 0)
+			 *	_puts("its here./shell: No such file or directory\n");
+			 */
 		}
 	}
 	free(nc);
