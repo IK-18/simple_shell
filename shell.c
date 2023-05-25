a2f91192242de14fc9370e9b31a3c1f711e24de0
IK-18 <ik.arigbe.osula@gmail.com>
2023-05-25 04:30:18 +0100

    troubleshoot

--- shell.c
+++ shell.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[], char *env[])
 				break;
 			}
 			inbt = inbuilt(av[0]);
-			printf(inbt);
+			printf("%d", inbt);
 			nc = fchk(av[0]);
 			if (inbt == 0 && nc != NULL)
 				av[0] = nc;
